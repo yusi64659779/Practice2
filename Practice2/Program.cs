b1fbf0c16f8a15325dd79d7233d9b2f281d08ba9
yusiwei <yusiwei@mail.usf.edu>
2019-09-10 20:46:50 -0400

    P2

---
+++ Practice2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years experiance do you have in professional programming:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+                Console.WriteLine("You have " + i + " years of experiences");
+        }
+    }
+}
